c7c8d80427aedede81e7cf9ebc4cb84a0cc125cf
Nauman Badar <nauman.gwt@gmail.com>
2011-03-31 14:21:30 +0200

    delivery status added to Email.java

--- src/Email/Email.java
+++ src/Email/Email.java
@@ -3,6 +3,11 @@ package Email;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+/**
+ * @author Nauman Badar <nauman.gwt@gmail.com>
+ * @created Mar 31, 2011
+ * 
+ */
 public class Email {
 
 	private String _from;
@@ -14,7 +19,7 @@ public class Email {
 
 	private String _submissionTime;
 	private String _deliveryTime;
-//	private String _deliveryStatus;
+	private String _deliveryStatus;
 
 	/**
 	 * @param _to
@@ -24,7 +29,7 @@ public class Email {
 	 * @param _delay
 	 * @param _message
 	 */
-	public Email(String _to, String _from,  String _subject, String smtpServer, int _delay, String _message) {
+	public Email(String _to, String _from, String _subject, String smtpServer, int _delay, String _message) {
 		super();
 		this._from = _from;
 		this._to = _to;
@@ -37,43 +42,43 @@ public class Email {
 
 		this._delay = _delay;
 		this._message = _message;
+
 		Calendar calendar = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("HH:MM:ss");
-		
+
 		_submissionTime = sdf.format(calendar.getTime());
-//		_deliveryTime.add(Calendar.SECOND, _delay);
 		calendar.add(Calendar.SECOND, _delay);
 		_deliveryTime = sdf.format(calendar.getTime());
+		_deliveryStatus = "PENDING";
 	}
 
-//	public Email() {
-//
-////		_submissionTime = Calendar.getInstance();
-//
-//	}
+	// public Email() {
+	//
+	// // _submissionTime = Calendar.getInstance();
+	//
+	// }
 
 	/**
 	 * @return the _submissionTime
 	 */
-//	public String get_submissionTime() {
-//		return _submissionTime;
-//	}
+	// public String get_submissionTime() {
+	// return _submissionTime;
+	// }
 
 	/**
 	 * @param _submissionTime
 	 *            the _submissionTime to set
 	 */
-//	public void set_submissionTime(Calendar _submissionTime) {
-//		this._submissionTime = _submissionTime;
-//	}
+	// public void set_submissionTime(Calendar _submissionTime) {
+	// this._submissionTime = _submissionTime;
+	// }
 
 	/**
 	 * @return the _deliveryTime
 	 */
-//	public String get_deliveryTime() {
-//		return _deliveryTime;
-//	}
-
+	// public String get_deliveryTime() {
+	// return _deliveryTime;
+	// }
 
 	/**
 	 * @param _deliveryTime
@@ -84,19 +89,19 @@ public class Email {
 	}
 
 	/**
-	 * @return the _deliveryStatus
-//	 */
-//	public String get_deliveryStatus() {
-//		return _deliveryStatus;
-//	}
-//
-//	/**
-//	 * @param _deliveryStatus
-//	 *            the _deliveryStatus to set
-//	 */
-//	public void set_deliveryStatus(String _deliveryStatus) {
-//		this._deliveryStatus = _deliveryStatus;
-//	}
+	 * @return the _deliveryStatus //
+	 */
+	public String get_deliveryStatus() {
+		return _deliveryStatus;
+	}
+
+	/**
+	 * @param _deliveryStatus
+	 *            the _deliveryStatus to set
+	 */
+	public void set_deliveryStatus(String _deliveryStatus) {
+		this._deliveryStatus = _deliveryStatus;
+	}
 
 	/**
 	 * @return the _from
@@ -109,9 +114,9 @@ public class Email {
 	 * @param _from
 	 *            the _from to set
 	 */
-//	public void set_from(String _from) {
-//		this._from = _from;
-//	}
+	// public void set_from(String _from) {
+	// this._from = _from;
+	// }
 
 	/**
 	 * @return the _to
@@ -120,13 +125,13 @@ public class Email {
 		return _to;
 	}
 
-//	/**
-//	 * @param _to
-//	 *            the _to to set
-//	 */
-//	public void set_to(String _to) {
-//		this._to = _to;
-//	}
+	// /**
+	// * @param _to
+	// * the _to to set
+	// */
+	// public void set_to(String _to) {
+	// this._to = _to;
+	// }
 
 	/**
 	 * @return the _subject
@@ -139,9 +144,9 @@ public class Email {
 	 * @param _subject
 	 *            the _subject to set
 	 */
-//	public void set_subject(String _subject) {
-//		this._subject = _subject;
-//	}
+	// public void set_subject(String _subject) {
+	// this._subject = _subject;
+	// }
 
 	/**
 	 * @return the _smtpServer
@@ -154,9 +159,9 @@ public class Email {
 	 * @param _smtpServer
 	 *            the _smtpServer to set
 	 */
-//	public void set_smtpServer(String _smtpServer) {
-//		this._smtpServer = _smtpServer;
-//	}
+	// public void set_smtpServer(String _smtpServer) {
+	// this._smtpServer = _smtpServer;
+	// }
 
 	/**
 	 * @return the _delay
@@ -169,9 +174,9 @@ public class Email {
 	 * @param _delay
 	 *            the _delay to set
 	 */
-//	public void set_delay(int _delay) {
-//		this._delay = _delay;
-//	}
+	// public void set_delay(int _delay) {
+	// this._delay = _delay;
+	// }
 
 	/**
 	 * @return the _message
@@ -184,23 +189,23 @@ public class Email {
 	 * @param _message
 	 *            the _message to set
 	 */
-//	public void set_message(String _message) {
-//		this._message = _message;
-//	}
+	// public void set_message(String _message) {
+	// this._message = _message;
+	// }
 
 	public String toString() {
 		return "FROM:" + _from + " " + "TO:" + _to + " " + "SUBJECT:" + _subject + " " + "SMTP_SERVER:" + _smtpServer + " " + "DELAY:" + _delay + " " + "MESSAGE:" + _message;
 	}
 
 	public String getSubmissionTimeString() {
-//		SimpleDateFormat sdf = new SimpleDateFormat("HH:MM:ss");
-//		return sdf.format(_submissionTime.getTime());
-	return _submissionTime;
+		// SimpleDateFormat sdf = new SimpleDateFormat("HH:MM:ss");
+		// return sdf.format(_submissionTime.getTime());
+		return _submissionTime;
 	}
 
 	public String getDeliveryTimeString() {
-//		SimpleDateFormat sdf = new SimpleDateFormat("HH:MM:ss");
-//		return sdf.format(_deliveryTime.getTime());
-	return _deliveryTime;
+		// SimpleDateFormat sdf = new SimpleDateFormat("HH:MM:ss");
+		// return sdf.format(_deliveryTime.getTime());
+		return _deliveryTime;
 	}
 }
--- src/SMTPClient/SMTPClient.java
+++ src/SMTPClient/SMTPClient.java
@@ -11,143 +11,129 @@ import java.net.UnknownHostException;
 
 import Email.Email;
 
-public class SMTPClient{
-	
-	public String sendEmail(Email email) throws UnknownHostException, IOException {
+public class SMTPClient {
+
+	public static String sendEmail(Email email) throws UnknownHostException, IOException {
 
 		String m = null;
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		      
+
 		PrintStream out = System.out;
-		      
-		     
-		         Socket c = new Socket("mail.ik2213.lab",25);
-		         
-		         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(
-		            c.getOutputStream()));
-		         BufferedReader r = new BufferedReader(new InputStreamReader(
-		            c.getInputStream()));
-		        
-		         
-		         //for the delay
-		         /*for (int i = 0; i < delay; i++)
-		         {
-		          try
-		              {
-		              Thread.sleep(1000);        
-		              }
-		           catch (InterruptedException ie)
-		              {
-		            System.out.println(ie.getMessage());
-		              }
-		         }*/
-		         
-		         //For Sending the HELO Message
-		         String hostName = email.get_smtpServer();
-		         //String hostName = " gmani.mail.ik2213.lab";
-		         
-		              
-		         w.write("EHLO "+hostName);
-		         w.newLine();
-		         int i =8;
-		         do
-		         {
-		         w.flush();
-		         m=r.readLine(); out.println(m);
-		         i--;
-		         }while(i!=0);
-		         ;
-		         //For Sending The From Address
-		         String from = email.get_from();
-		         //String from = "<gmani@mail.ik2213.lab>";
-		         out.println(from);
-		         String mailfrom = "MAIL FROM: " +"<"+ from +">";
-		         w.write(mailfrom);
-		         w.newLine();
-		         w.flush();
-		        m=r.readLine();out.println(m);
-		       
-		        //for checking the correctness of from address
-		        if(!m.equals("250 2.1.0 Ok"))
-		       {
-		        	out.println("Error in From Part");
-		        	return m;
-		       }
-		         //For Sending The TO Address
-		         
-		        String to = email.get_to();
-		        //String to = " <gmani@mail.ik2213.lab>";
-		         w.write("RCPT TO: "+"<"+to+">");
-		         w.newLine();
-		         w.flush();
-		         m=r.readLine();out.println(m);
-		       //for checking the correctness of from address
-			        if(!m.equals("250 2.1.5 Ok"))
-			       {
-			        	out.println("Error in To Part");
-			        	return m;
-			       }
-		         
-			     String message = email.get_message();
-			     //String message = "hai how are you.";
-			     String subject= email.get_subject();	
-			     //String subject= "hello";
-		         
-		         w.write("DATA");
-		         w.newLine();
-		         w.flush();
-		      // send header As a part of Message
-		         /*w.write("From: " + from + "\r\n");
-		         w.write("To: " + to + "\r\n");
-		         w.write("Subject: " + subject + "\r\n");
-		         w.write("\r\n"); // end header*/
 
-		         w.write("MIME-Version: 1.1"+ "\r\n");
-		         w.write("Content-Type: text/plain"+ "\r\n");
-		         w.write("Content-Transfer-Encoding: quoted-printable"+"\r\n");
-		         //w.write("\r\n"); // end header
-		         w.flush();
-		         String m1=r.readLine();out.println(m1);
-		     
-		         // send header As a part of Message
-		         w.write("From: " + from + "\r\n");
-		         w.write("To: " + to + "\r\n");
-		         //w.write("Subject: " + subject + "\r\n");
-		         w.write("Subject: =?ISO-8859-1?Q?" + subject + "?=\r\n");
-		         w.write("\r\n"); // end header
-		        
-		         // The Body of the mail		         
-		         w.write(message + "\r\n");
+		Socket c = new Socket("mail.ik2213.lab", 25);
 
-		         // end data with <CR/LF>.<CR/LF>
-		         w.write("\r\n.\r\n.....");
-		         w.flush();
-		         
-		         m=r.readLine(); out.println(m);
-		         if(!m.regionMatches(0, "2.0.0 Ok: queued as",0,10))
-			       {
-			        	out.println("Error in Body");
-			        	return m;
-			       }
-		         // To close the SMTP connection
-		         String quit = "QUIT";
-		         w.write(quit);
-		         w.newLine();
-		         w.flush();
-		         
-		         //m=r.readLine();out.println(m);
-		         
-		        
-		         w.close();
-		         r.close();
-		         c.close();
-		         
-		     
+		BufferedWriter w = new BufferedWriter(new OutputStreamWriter(c.getOutputStream()));
+		BufferedReader r = new BufferedReader(new InputStreamReader(c.getInputStream()));
+
+		// for the delay
+		/*
+		 * for (int i = 0; i < delay; i++) { try { Thread.sleep(1000); } catch
+		 * (InterruptedException ie) { System.out.println(ie.getMessage()); } }
+		 */
+
+		// For Sending the HELO Message
+		String hostName = email.get_smtpServer();
+		// String hostName = " gmani.mail.ik2213.lab";
+
+		w.write("EHLO " + hostName);
+		w.newLine();
+		int i = 8;
+		do {
+			w.flush();
+			m = r.readLine();
+			out.println(m);
+			i--;
+		} while (i != 0);
+		;
+		// For Sending The From Address
+		String from = email.get_from();
+		// String from = "<gmani@mail.ik2213.lab>";
+		out.println(from);
+		String mailfrom = "MAIL FROM: " + "<" + from + ">";
+		w.write(mailfrom);
+		w.newLine();
+		w.flush();
+		m = r.readLine();
+		out.println(m);
+
+		// for checking the correctness of from address
+		if (!m.equals("250 2.1.0 Ok")) {
+			out.println("Error in From Part");
 			return m;
+		}
+		// For Sending The TO Address
+
+		String to = email.get_to();
+		// String to = " <gmani@mail.ik2213.lab>";
+		w.write("RCPT TO: " + "<" + to + ">");
+		w.newLine();
+		w.flush();
+		m = r.readLine();
+		out.println(m);
+		// for checking the correctness of from address
+		if (!m.equals("250 2.1.5 Ok")) {
+			out.println("Error in To Part");
+			return m;
+		}
+
+		String message = email.get_message();
+		// String message = "hai how are you.";
+		String subject = email.get_subject();
+		// String subject= "hello";
+
+		w.write("DATA");
+		w.newLine();
+		w.flush();
+		// send header As a part of Message
+		/*
+		 * w.write("From: " + from + "\r\n"); w.write("To: " + to + "\r\n");
+		 * w.write("Subject: " + subject + "\r\n"); w.write("\r\n"); // end
+		 * header
+		 */
+
+		w.write("MIME-Version: 1.1" + "\r\n");
+		w.write("Content-Type: text/plain" + "\r\n");
+		w.write("Content-Transfer-Encoding: quoted-printable" + "\r\n");
+		// w.write("\r\n"); // end header
+		w.flush();
+		String m1 = r.readLine();
+		out.println(m1);
+
+		// send header As a part of Message
+		w.write("From: " + from + "\r\n");
+		w.write("To: " + to + "\r\n");
+		// w.write("Subject: " + subject + "\r\n");
+		w.write("Subject: =?ISO-8859-1?Q?" + subject + "?=\r\n");
+		w.write("\r\n"); // end header
+
+		// The Body of the mail
+		w.write(message + "\r\n");
+
+		// end data with <CR/LF>.<CR/LF>
+		w.write("\r\n.\r\n.....");
+		w.flush();
+
+		m = r.readLine();
+		out.println(m);
+		if (!m.regionMatches(0, "2.0.0 Ok: queued as", 0, 10)) {
+			out.println("Error in Body");
+			return m;
+		}
+		// To close the SMTP connection
+		String quit = "QUIT";
+		w.write(quit);
+		w.newLine();
+		w.flush();
+
+		// m=r.readLine();out.println(m);
+
+		w.close();
+		r.close();
+		c.close();
+
+		return m;
 
 	}
-
-	
 
 }
