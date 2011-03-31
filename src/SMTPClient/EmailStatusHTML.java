f4d98865f80521dad2dbfc42450c9ac62d2640e1
Nauman Badar <nauman.gwt@gmail.com>
2011-03-31 23:34:21 +0200

    Status Page is integrated :)

--- src/SMTPClient/EmailStatusHTML.java
+++ src/SMTPClient/EmailStatusHTML.java
@@ -14,10 +14,10 @@ public class EmailStatusHTML {
 		htmlData.append("<BODY>");
 		
 		htmlData.append("<table style =\"color: #a52a2a\" border = 10 cellspacing = 1 cellpadding = 5 bgcolor = \"fa8072\" bordercolor = \"b22222\">");
-		htmlData.append("<tr align = center><td>From</td<td>To</td><td>Subject</td><td>Sent Time</td><td>Delivery Time</td><td>Status</td></tr>");
+		htmlData.append("<tr align = center><td>From</td<td>To</td><td>Subject</td><td>Sent Time</td><td>Expected Delivery Time</td><td>Status</td></tr>");
 		for (Email email : emailList) {
 			
-			htmlData.append("<tr><td>"+email.get_from()+"</td><td>"+email.get_to()+"</td><td>"+email.get_subject()+"</td><td>"+email.getSubmissionTimeString()+"</td><td>"+email.getDeliveryTimeString()+"</td><td>"+email.get_deliveryStatus()+"</td></tr>");
+			htmlData.append("<tr><td>"+email.get_from()+"</td><td>"+email.get_to()+"</td><td>"+email.get_originalSubject()+"</td><td>"+email.getSubmissionTimeString()+"</td><td>"+email.getDeliveryTimeString()+"</td><td>"+email.get_deliveryStatus()+"</td></tr>");
 			
 		}
 		
--- src/WebServer/GetStatusHandler.java
+++ src/WebServer/GetStatusHandler.java
@@ -6,6 +6,9 @@ package WebServer;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 
+import SMTPClient.EmailStatusHTML;
+import StateManager.StateManager;
+
 /**
  * @author Nauman Badar <nauman.gwt@gmail.com>
  * @created Mar 31, 2011
@@ -19,8 +22,12 @@ public class GetStatusHandler {
 	 * @param completeHeader
 	 */
 	public static void handle(BufferedReader inputBufferReader, PrintWriter outputPrintWriter, StringBuilder completeHeader) {
-		// TODO Auto-generated method stub
+		StateManager stateManager  = StateManager.INSTANCE;
+		String statusString = EmailStatusHTML.spitHTML(stateManager.getEmailList());
 		
+		outputPrintWriter.println("HTTP/1.1 200 OK\r\n");
+//		outputPrintWriter.println("<HTML><HEAD><TITLE>Hello from MANEN</TITLE></HEAD><BODY><H1>Pay your bill.</H1></BODY></HTML>");
+		outputPrintWriter.print(statusString);
 	}
 
 }
